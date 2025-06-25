30de16ea4c8223895181887f2baba5dfad581ea4
Juanes <juanespachon1203@gmail.com>
2025-06-24 21:58:14 -0500

    feat(home): add desktop home structure

--- client/src/app/features/home/home.ts
+++ client/src/app/features/home/home.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit, signal, Signal } from '@angular/core';
+import { map, Subject, takeUntil } from 'rxjs';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +8,26 @@ import { Component } from '@angular/core';
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
-export class Home {
+export class Home implements OnInit, OnDestroy {
+
+  private breakpoint = inject(BreakpointObserver);
+
+  readonly isMobile = signal(true);
+  private destroy$ = new Subject<void>();
+
+  ngOnInit() {
+    this.breakpoint
+      .observe('(min-width: 1024px)')
+      .pipe(
+        map(state => !state.matches),
+        takeUntil(this.destroy$)
+      )
+      .subscribe(isMobile => this.isMobile.set(isMobile));
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 
 }
