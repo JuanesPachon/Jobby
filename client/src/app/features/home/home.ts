d7b35e8bf0c37d00460efdba75480cd8a18307fc
Juanes <juanespachon1203@gmail.com>
2025-07-02 20:20:30 -0500

    perf(home): Optimize how main images render

--- client/src/app/features/home/home.ts
+++ client/src/app/features/home/home.ts
@@ -3,15 +3,16 @@ import { HomeNavbar } from '../../shared/home-navbar/home-navbar';
 import { HomeFooter } from '../../shared/home-footer/home-footer';
 import { HomeRating } from './components/home-rating/home-rating';
 import { HomeCollage } from './components/home-collage/home-collage';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [HomeNavbar, HomeFooter, HomeRating, HomeCollage],
+  imports: [HomeNavbar, HomeFooter, HomeRating, HomeCollage, NgOptimizedImage],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
 
 
-  
+
 }
