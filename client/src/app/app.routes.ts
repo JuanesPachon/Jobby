8dd00cdb5dbe1776d49c30b0612e908ac8665de5
Juanes <juanespachon1203@gmail.com>
2025-07-15 13:26:06 -0500

    feat(routes): add register route

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Home } from './features/home/home';
+import { Register } from './features/auth/register/register';
 
 export const routes: Routes = [
-    {path: '', component: Home}
+    {path: '', component: Home},
+    {path: 'register', component: Register}
 ];
--- client/src/app/features/home/home.ts
+++ client/src/app/features/home/home.ts
@@ -4,10 +4,11 @@ import { HomeFooter } from '../../shared/home-footer/home-footer';
 import { HomeRating } from './components/home-rating/home-rating';
 import { HomeInfo } from './components/home-info/home-info';
 import { NgOptimizedImage } from '@angular/common';
+import { RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [HomeNavbar, HomeFooter, HomeRating, HomeInfo, NgOptimizedImage],
+  imports: [HomeNavbar, HomeFooter, HomeRating, HomeInfo, NgOptimizedImage, RouterLinkWithHref],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
