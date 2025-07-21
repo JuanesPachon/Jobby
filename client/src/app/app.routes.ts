18a17d539a6d717a566b12acd1bd1efc9982d6fc
Juanes <juanespachon1203@gmail.com>
2025-07-21 14:54:47 -0500

    feat(dashboard): add dashboard component with routing

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './features/home/home';
 import { Register } from './features/auth/register/register';
+import { Dashboard } from './features/dashboard/dashboard';
 
 export const routes: Routes = [
     {path: '', component: Home},
-    {path: 'register', component: Register}
+    {path: 'register', component: Register},
+    {path: 'dashboard', component: Dashboard}
 ];
