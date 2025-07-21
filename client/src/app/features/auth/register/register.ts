34cce1c477fc886e157c03fc0e2a8a871bbfc8e8
Juanes <juanespachon1203@gmail.com>
2025-07-20 23:20:39 -0500

    feat(auth): update registration and form validation

--- client/src/app/features/auth/register/register.ts
+++ client/src/app/features/auth/register/register.ts
@@ -12,47 +12,35 @@ import { RegisterRequest } from './models/RegisterRequest';
 export class Register {
 
   private userService = inject(UserService);
-
-  nichille = 'NGAP'
   
   signUpForm = new FormGroup({
-    firstName: new FormControl('', [Validators.required]),
-    lastName: new FormControl('', [Validators.required]),
+    firstName: new FormControl('', [Validators.required, Validators.pattern(/^[A-Za-z]+$/)]),
+    lastName: new FormControl('', [Validators.required, Validators.pattern(/^[A-Za-z]+$/)]),
     birthDay: new FormControl('', [Validators.required]),
     idType: new FormControl('', [Validators.required]),
-    idNumber: new FormControl('', [Validators.required]),
+    idNumber: new FormControl('', [Validators.required, Validators.pattern(/^\d{10}$/)]),
     email: new FormControl('', [Validators.required, Validators.email]),
-    phone: new FormControl('', [Validators.required, Validators.pattern('^\\+?[0-9]{10,15}$')]),
+    phone: new FormControl('', [Validators.required, Validators.pattern(/^\d{10}$/)]),
     password: new FormControl('', [Validators.required, Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^A-Za-z\\d]).{8,}$')]),
     termsAndConditions: new FormControl(false, [Validators.requiredTrue])
   });
 
-  attemptSignUp() {
-    console.log('Form Data:', this.signUpForm.value);
+  attemptSignUp(event: Event) {
+
+    event.preventDefault();
 
     if (this.signUpForm.valid) {
-      const formData = this.signUpForm.value as {
-        firstName: string;
-        lastName: string;
-        birthDay: string;
-        idType: string;
-        idNumber: string;
-        email: string;
-        phone: string;
-        password: string;
-        termsAndConditions: boolean;
-      };
-      console.log('Form Data:', formData)
+      const formData = this.signUpForm.value
       const registerRequest : RegisterRequest = {
-        first_name: formData.firstName,
-        last_name: formData.lastName,
-        birth_date: formData.birthDay,
-        doc_type: formData.idType,
-        doc_number: formData.idNumber,
-        email: formData.email,
-        phone: formData.phone,
-        password: formData.password,
-        accepted_terms: formData.termsAndConditions
+        first_name: formData.firstName!,
+        last_name: formData.lastName!,
+        birth_date: formData.birthDay!,
+        doc_type: formData.idType!,
+        doc_number: formData.idNumber!,
+        email: formData.email!,
+        phone: formData.phone!,
+        password: formData.password!,
+        accepted_terms: formData.termsAndConditions!
       }
       this.userService.attemptSignUp(registerRequest).subscribe({
         next: (response) => {
@@ -67,4 +55,5 @@ export class Register {
     }
     this.signUpForm.reset();
   }
+
 }
--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://62ac3e3e-5d6a-4f0b-93b7-97c4910e0100.mock.pstmn.io/api/v1',
+    apiUrl: 'http://localhost:3000/api/v1',
 };
