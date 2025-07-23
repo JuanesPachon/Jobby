1cbb865a17f2f238ea94b3b8ae5d3dbc20ff271a
Juanes <juanespachon1203@gmail.com>
2025-07-23 12:26:17 -0500

    feat(navbar): add routerLink to login links in home-navbar

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Home } from './features/home/home';
 import { Register } from './features/auth/register/register';
 import { Dashboard } from './features/dashboard/dashboard';
+import { Login } from './features/auth/login/login';
 
 export const routes: Routes = [
     {path: '', component: Home},
     {path: 'register', component: Register},
+    {path: 'login', component: Login},
     {path: 'dashboard', component: Dashboard}
 ];
