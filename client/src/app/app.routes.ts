962180792b8b8c48a928246141efbdca721ad23c
Juanes <juanespachon1203@gmail.com>
2025-07-26 17:57:11 -0500

    feat(auth): implement auth and route protection with login request

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,10 +3,11 @@ import { Home } from './features/home/home';
 import { Register } from './features/auth/register/register';
 import { Dashboard } from './features/dashboard/dashboard';
 import { Login } from './features/auth/login/login';
+import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
     {path: '', component: Home},
     {path: 'register', component: Register},
     {path: 'login', component: Login},
-    {path: 'dashboard', component: Dashboard}
+    {path: 'dashboard', canActivate: [authGuard] ,component: Dashboard}
 ];
