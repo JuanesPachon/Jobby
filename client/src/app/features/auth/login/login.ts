e691ad426621c70adc70902a831bcd8dd5a2f840
Juanes <juanespachon1203@gmail.com>
2025-07-26 17:59:18 -0500

    feat(auth): implement login form validation, authentication and error handling

--- client/src/app/features/auth/login/login.ts
+++ client/src/app/features/auth/login/login.ts
@@ -1,11 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { AuthService } from '../../../core/services/auth.service';
+import { LoginRequest } from './interfaces/LoginRequest';
+import { Router, RouterLink } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink, NgClass],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
+
 export class Login {
 
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required])
+  });
+
+  isLoading = signal<Boolean>(false);
+  authError = signal<Boolean>(false);
+  authErrorMessage = signal<string>('');
+
+  attemptLogin(event: Event) {
+    event.preventDefault();
+
+    if (this.loginForm.valid) {
+
+      this.isLoading.update(value => !value);
+
+      const formData = this.loginForm.value;
+
+      const loginRequest: LoginRequest = {
+        email: formData.email!,
+        password: formData.password!
+      };
+
+      this.authService.attemptLogin(loginRequest).subscribe({
+        next: (response) => {
+          this.router.navigate(['/dashboard']);
+        },
+        error: (error) => {
+          this.authErrorMessage.set(error.status === 401 ? 'Credenciales incorrectas, vuelve a intentarlo' : error.status === 500 ? 'Error del servidor, vuelve a intentarlo mas tarde' : '');
+          this.authError.update(value => !value);
+          this.isLoading.update(value => !value);
+        }
+      });
+
+    } else {
+      this.isLoading.update(value => !value);
+    }
+  }
 }
