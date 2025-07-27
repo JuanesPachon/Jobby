aad423e57d7c9936ee4fbc3a3af7614b4a14f696
Heidy <lyesenia1103@gmail.com>
2025-07-26 19:19:07 -0500

    feat(component): create DashboardNavbar component with drop-down menu functionality

--- client/src/app/shared/dashboard-navbar/dashboard-navbar.ts
+++ client/src/app/shared/dashboard-navbar/dashboard-navbar.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
-
+import { NgIf } from '@angular/common';
 @Component({
   selector: 'app-dashboard-navbar',
-  imports: [],
+  standalone: true,
+  imports: [NgIf],
   templateUrl: './dashboard-navbar.html',
   styleUrl: './dashboard-navbar.css'
 })
 export class DashboardNavbar {
+  isDropdownOpen = false;
 
+  toggleDropdown(): void {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  closeDropdown(): void {
+    this.isDropdownOpen = false;
+  }
 }
