d7ca4298b3dd182b82fb1ac920b4e08b205fc4e1
Juanes <juanespachon1203@gmail.com>
2025-08-06 09:41:42 -0500

    feat(auth): add recoveryPassword components

---
+++ client/src/app/feature/auth/password-recovery/components/request-code/request-code.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-request-code',
+  imports: [],
+  templateUrl: './request-code.html',
+  styleUrl: './request-code.css'
+})
+export class RequestCode {
+
+}
---
+++ server/src/middlewares/validateResetPassword.ts
@@ -0,0 +1,35 @@
+import { check } from "express-validator";
+
+export const resetPasswordValidations = [
+  check("email")
+    .notEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("Please enter a valid email address")
+    .isLength({ max: 100 })
+    .withMessage("Email must not exceed 100 characters"),
+
+  check("newPassword")
+    .notEmpty()
+    .withMessage("New password is required")
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters long")
+    .matches(/\d/)
+    .withMessage("Password must contain at least one number")
+    .matches(/[a-z]/)
+    .withMessage("Password must contain at least one lowercase letter")
+    .matches(/[A-Z]/)
+    .withMessage("Password must contain at least one uppercase letter")
+    .matches(/[!@#$%^&*(),.?":{}|<>]/)
+    .withMessage("Password must contain at least one special character"),
+
+  check("confirmPassword")
+    .notEmpty()
+    .withMessage("Password confirmation is required")
+    .custom((value, { req }) => {
+      if (value !== req.body.newPassword) {
+        throw new Error("Password confirmation does not match new password");
+      }
+      return true;
+    }),
+];
