c6b4ba7e32490cc9a553f099b17189ab39ae09d6
SggtiagoDev <sggtiago@gmail.com>
2025-08-07 15:48:38 -0500

    feat(password-recovery): add password recovery views

--- client/src/app/features/auth/password-recovery/password-recovery.ts
+++ client/src/app/features/auth/password-recovery/password-recovery.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { RequestCode } from "./components/request-code/request-code";
+import { VerifyCode } from "./components/verify-code/verify-code";
+import { ResetPassword } from "./components/reset-password/reset-password";
 
 @Component({
   selector: 'app-password-recovery',
-  imports: [],
+  imports: [RequestCode, VerifyCode, ResetPassword],
   templateUrl: './password-recovery.html',
   styleUrl: './password-recovery.css'
 })
 export default class PasswordRecovery {
-
+  step = signal<number>(1)
 }
