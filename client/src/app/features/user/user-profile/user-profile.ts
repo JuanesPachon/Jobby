1b122ce8c7e26e9ac1b6662aea986049c2147cbf
Juanes <juanespachon1203@gmail.com>
2025-08-21 14:46:12 -0500

    feat(user): add user profile route and component; update dashboard navbar for profile link

--- client/src/app/features/user/user-profile/user-profile.ts
+++ client/src/app/features/user/user-profile/user-profile.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   templateUrl: './user-profile.html',
   styleUrl: './user-profile.css'
 })
-export class UserProfile {
+export default class UserProfile {
 
 }
