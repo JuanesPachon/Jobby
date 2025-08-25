1d60bc247df5db7e850585d8db7ed40376f820e6
NGAP <niki.arenas@gmail.com>
2025-08-24 20:05:27 -0500

    feat: add user profile view

--- client/src/app/features/user/user-profile/user-profile.ts
+++ client/src/app/features/user/user-profile/user-profile.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { DashboardNavbar } from '../../../shared/dashboard-navbar/dashboard-navbar';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-user-profile',
-  imports: [],
+  imports: [DashboardNavbar, RouterLink],
   templateUrl: './user-profile.html',
   styleUrl: './user-profile.css'
 })
