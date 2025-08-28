3191dd5eb934b85b1821700fd1b11816c0871c35
camila <rmariacamila828@gmail.com>
2025-08-28 18:23:25 -0500

    Feat(profile): implement user profile data consumed from server

---
+++ client/src/app/features/user/interfaces/userData.interface.ts
@@ -0,0 +1,41 @@
+export interface UserData {
+  id?: number
+  first_name?: string
+  last_name?: string
+  email?: string
+  phone?: string
+  created_at?: string
+  updated_at?: string
+  profile?: Profile
+  experiences?: Experience[]
+  skills?: Skill[]
+  documents?: Document[]
+}
+
+export interface Profile {
+  photo_url: string
+  description: string
+  created_at: string
+  updated_at: string
+}
+
+export interface Experience {
+  id: number
+  title: string
+  location: string
+  start_date: string
+  end_date?: string
+  created_at: string
+  updated_at: string
+}
+
+export interface Skill {
+  id: number
+  skill_name: string
+}
+
+export interface Document {
+  id: number
+  file_url: string
+  uploaded_at: string
+}
--- client/src/app/features/user/services/user.service.ts
+++ client/src/app/features/user/services/user.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
-
-  constructor() { }
+  private apiUrl = environment.apiUrl;
+  private http = inject(HttpClient);
+  getUserProfile(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/user/profile` ,
+    { withCredentials: true });
+  }
 }
+
