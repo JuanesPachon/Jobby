344be80d98c6bc261b8356a8e842dffdb813433f
Juanes <juanespachon1203@gmail.com>
2025-09-01 14:41:08 -0500

    feat(tasks): add main tasks components

---
+++ client/src/app/features/tasks/post-task/post-task.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-public-task',
+  imports: [],
+  templateUrl: './post-task.html',
+  styleUrl: './post-task.css'
+})
+export class PostTask {
+
+}
