4522b816454e0492ef941e0e2e174ba2afda0252
Juanes <juanespachon1203@gmail.com>
2025-11-20 18:10:57 -0500

    feat(vercel): add initial Vercel configuration for deployment

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000/api/v1',
+    apiUrl: 'https://server-jobby.vercel.app/api/v1',
     supabaseStorageUrl: 'https://yelcwjxpzmknkzedgpwe.supabase.co/storage/v1/object/public/Jobby_files/',
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://jobby-qrmu.onrender.com/api/v1',
+    apiUrl: 'https://server-jobby.vercel.app/api/v1',
     supabaseStorageUrl: 'https://yelcwjxpzmknkzedgpwe.supabase.co/storage/v1/object/public/Jobby_files/',
 };
